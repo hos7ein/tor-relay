57b5b5b80d2c3a2f4ea4a126b7a217ffb78e3e33
hos7ein <hossein.a97@gmail.com>
2026-03-29 17:47:13 +0200

    fix script

--- run.sh
+++ run.sh
@@ -17,6 +17,8 @@ for relaytype in bridge middle exit; do
     file="/etc/tor/torrc.${relaytype}"
     if [ -f "$file" ]; then
         sed -i "s/RELAY_NICKNAME/${RELAY_NICKNAME}/g" "$file"
+        sed -i "s/CONTACT_GPG_FINGERPRINT/${CONTACT_GPG_FINGERPRINT}/g" "$file"
+	    sed -i "s/CONTACT_NAME/${CONTACT_NAME}/g" "$file"
         sed -i "s/CONTACT_EMAIL/${CONTACT_EMAIL}/g" "$file"
         sed -i "s/RELAY_BANDWIDTH_RATE/${RELAY_BANDWIDTH_RATE}/g" "$file"
         sed -i "s/RELAY_BANDWIDTH_BURST/${RELAY_BANDWIDTH_BURST}/g" "$file"
